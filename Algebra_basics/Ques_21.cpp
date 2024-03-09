d4cc582d0db3e215cab278be8f7688a072490aff
barbariansubhkaran <subhkaran.foam@gmail.com>
2024-03-09 07:51:54 +0530

    first

---
+++ Algebra_basics/Ques_21.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+         int a = 4, b = 3;
+
+         cout << "Answer is : " << (a * pow(b,2)) << endl;
+
+      return 0;
+}
